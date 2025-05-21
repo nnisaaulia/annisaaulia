290924b1243bc94e8b2ea8ea61ab66934dbc381d
nnisaaulia <nsaaliana@gmail.com>
2025-05-21 09:23:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("🧇 My New APP 🗯")
+st.title("🗯 My New APP")
 st.write(
-    "lets go"
+    "lets go 💥"
 )
 st.image("0f78443ffc699fc7e6ffd6a362c2b4cc.jpg", width=200)
